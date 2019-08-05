72a3dac7f8c91088eab9243c9a66a07c8790da5a
Itanik <yan.xhrome@gmail.com>
2019-08-05 19:12:39 +0300

    Add NewsFragment, necessary functions and layouts
    Added fragment
    transition, backBtn transaction in MainActivity.
    Initialized news_fragment layout with news_list RecyclerView in NewsFragment.
    Base post markup created.

---
+++ app/src/main/java/com/itanik/keddit/NewsFragment.kt
@@ -0,0 +1,23 @@
+package com.itanik.keddit
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import kotlinx.android.synthetic.main.news_fragment.*
+
+class NewsFragment : Fragment() {
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        return container?.inflate(R.layout.news_fragment)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        news_list?.setHasFixedSize(true) // оптимизирует скорость разворачиваия списка
+        news_list?.layoutManager = LinearLayoutManager(context)
+    }
+}
