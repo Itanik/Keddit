21fa896752d2c0601d4e1e8fb2092e98ba288759
Itanik <yan.xhrome@gmail.com>
2019-08-19 02:59:10 +0300

    Stable. Create REST request and show first 10 real Reddit posts

--- app/src/main/java/com/itanik/keddit/features/news/NewsManager.kt
+++ app/src/main/java/com/itanik/keddit/features/news/NewsManager.kt
@@ -1,24 +1,26 @@
 package com.itanik.keddit.features.news
 
+import com.itanik.keddit.api.RestAPI
 import com.itanik.keddit.commons.RedditNewsItem
 import io.reactivex.Observable
 
-class NewsManager {
+class NewsManager(private val api: RestAPI = RestAPI()) {
 
-    fun getNews(): Observable<List<RedditNewsItem>> {
+    fun getNews(limit : Int = 10): Observable<List<RedditNewsItem>> {
         return Observable.create {
                 subscriber ->
-
-            val news = (1..10).map { RedditNewsItem(
-                    "Author$it",
-                    "Title $it",
-                    it, // number of comments
-                    1457207701L - it * 200000, // time
-                "https://picsum.photos/200/200?image=$it",
-                    "url"
-                )
+            val callResponse = api.getNews("", limit.toString())
+            val response = callResponse.execute()
+            if (response.isSuccessful) {
+                val news = response.body()?.data?.children?.map {
+                    val item = it.data
+                    RedditNewsItem(item.author, item.title, item.num_comments,
+                        item.created, item.thumbnail, item.url)
+                }
+                subscriber.onNext(news!!)
+            } else {
+                subscriber.onError(Throwable(response.message()))
             }
-            subscriber.onNext(news)
         }
     }
 }
