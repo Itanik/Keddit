5622093273ca8aecab8aef821e9053a324accf2f
Itanik <yan.xhrome@gmail.com>
2019-10-06 13:49:06 +0300

    add infinite scroll

--- app/src/main/java/com/itanik/keddit/NewsFragment.kt
+++ app/src/main/java/com/itanik/keddit/NewsFragment.kt
@@ -6,6 +6,8 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.google.android.material.snackbar.Snackbar
+import com.itanik.keddit.commons.InfiniteScrollListener
+import com.itanik.keddit.commons.RedditNews
 import com.itanik.keddit.commons.RxBaseFragment
 import com.itanik.keddit.commons.extensions.inflate
 import com.itanik.keddit.features.news.NewsManager
@@ -15,6 +17,7 @@ import kotlinx.android.synthetic.main.news_fragment.*
 
 class NewsFragment : RxBaseFragment() {
     private val newsManager by lazy { NewsManager() }
+    private var redditNews: RedditNews? = null
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
@@ -24,7 +27,10 @@ class NewsFragment : RxBaseFragment() {
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
         news_list?.setHasFixedSize(true) // оптимизирует скорость разворачиваия списка
-        news_list?.layoutManager = LinearLayoutManager(context)
+        val linearLayout = LinearLayoutManager(context)
+        news_list.layoutManager = linearLayout
+        news_list.clearOnScrollListeners()
+        news_list.addOnScrollListener(InfiniteScrollListener({ requestNews() }, linearLayout))
         initAdapter()
         if (savedInstanceState == null) {
             requestNews()
@@ -38,11 +44,12 @@ class NewsFragment : RxBaseFragment() {
     }
 
     private fun requestNews() {
-        val subscription = newsManager.getNews()
+        val subscription = newsManager.getNews(redditNews?.after ?: "")
             .subscribeOn(Schedulers.io())
             .subscribe (
                 { retrievedNews ->
-                    (news_list.adapter as NewsAdapter).addNews(retrievedNews)
+                    redditNews = retrievedNews
+                    (news_list.adapter as NewsAdapter).addNews(retrievedNews.news)
                 },
                 { e ->
                     Snackbar.make(news_list, e.message ?: "Something went wrong", Snackbar.LENGTH_LONG).show()
--- app/src/main/java/com/itanik/keddit/features/news/NewsManager.kt
+++ app/src/main/java/com/itanik/keddit/features/news/NewsManager.kt
@@ -1,23 +1,34 @@
 package com.itanik.keddit.features.news
 
 import com.itanik.keddit.api.RestAPI
+import com.itanik.keddit.commons.RedditNews
 import com.itanik.keddit.commons.RedditNewsItem
 import io.reactivex.Observable
 
+/**
+* Позволяет получать новости от Reddit API
+ * */
+
 class NewsManager(private val api: RestAPI = RestAPI()) {
 
-    fun getNews(limit : Int = 10): Observable<List<RedditNewsItem>> {
+    fun getNews(after: String, limit: String = "10"): Observable<RedditNews> {
         return Observable.create {
                 subscriber ->
-            val callResponse = api.getNews("", limit.toString())
+            val callResponse = api.getNews(after, limit)
             val response = callResponse.execute()
             if (response.isSuccessful) {
-                val news = response.body()?.data?.children?.map {
+                val dataResponse = response.body()?.data
+                val news = dataResponse?.children?.map {
                     val item = it.data
                     RedditNewsItem(item.author, item.title, item.num_comments,
                         item.created, item.thumbnail, item.url)
                 }
-                subscriber.onNext(news!!)
+                val redditNews = RedditNews(
+                    dataResponse?.after ?: "",
+                    dataResponse?.before ?: "",
+                    news!!)
+
+                subscriber.onNext(redditNews)
             } else {
                 subscriber.onError(Throwable(response.message()))
             }
